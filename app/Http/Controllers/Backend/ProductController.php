d9fb5013ac737386a3552d987b9950b0ff51882d
Munotasir2762 <muntasirpranto2762@gmail.com>
2025-02-24 22:30:59 +0600

    Product EDIT-UPDATE done

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -154,4 +154,106 @@ class ProductController extends Controller
         $subCategories = SubCategory::get();
         return view ('backend.product.edit', compact('product', 'categories', 'subCategories'));
     }
+
+    public function productUpdate (Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        if(isset($request->image)){
+            if($product->image && file_exists('backend/images/product/'.$product->image)){
+                unlink('backend/images/product/'.$product->image);
+            }
+
+            $imageName = rand().'-product-'.'.'.$request->image->extension(); // 85778-category-.jpg
+            $request->image->move('backend/images/product',$imageName);
+ 
+            $product->image = $imageName;
+        }
+
+        if(isset($request->galleryImage)){
+
+            $images = GalleryImage::where('product_id', $product->id)->get();
+
+            if($images->isNotEmpty()){
+                foreach($images as $singleImage){
+                    if($singleImage->image && file_exists('backend/images/gallerImage/'.$singleImage->image)){
+                        unlink('backend/images/gallerImage/'.$singleImage->image);
+                    }
+    
+                    $singleImage->delete();
+    
+                }
+    
+                foreach($request->galleryImage as $image){
+                    $galleryImage = new GalleryImage();
+    
+                    $galleryImage->product_id = $product->id;
+    
+                    $imageName = rand().'-galleryimage-'.'.'.$image->extension(); // 85778-galleryimage-.jpg
+                    $image->move('backend/images/gallerImage',$imageName);
+    
+                    $galleryImage->image = $imageName;
+                    $galleryImage->save();
+                }
+            }
+        }
+
+         //Add Color..
+         if(isset($request->color) && $request->color[0] != null){
+            $colors = Color::where('product_id', $product->id)->get();
+            if($colors->isNotEmpty()){
+                foreach($colors as $color){
+                    $color->delete();
+                }
+            }
+
+            foreach($request->color as $color_name){
+                $color = new Color();
+    
+                $color->product_id = $product->id;
+                $color->color_name = $color_name;
+    
+                $color->save();
+            }
+    
+        }
+
+        //Add Size..
+        if(isset($request->size) && $request->size[0] != null){
+            $sizes = Size::where('product_id',$product->id)->get();
+            
+            if($sizes->isNotEmpty()){
+                foreach($sizes as $size){
+                    $size->delete();
+                }
+            }
+
+            foreach($request->size as $size_name){
+                $size = new Size();
+    
+                $size->product_id = $product->id;
+                $size->size_name = $size_name;
+    
+                $size->save();
+            }
+        }
+
+        
+
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->cat_id = $request->cat_id;
+        $product->sub_cat_id = $request->sub_cat_id;
+        $product->sku_code = $request->sku_code;
+        $product->buying_price = $request->buying_price;
+        $product->regular_price = $request->regular_price;
+        $product->discount_price = $request->discount_price;
+        $product->qty = $request->qty;
+        $product->description = $request->description;
+        $product->policy = $request->policy;
+        $product->product_type = $request->product_type;
+
+        $product->save();
+        return redirect()->back();
+    }
 }
--- resources/views/backend/product/edit.blade.php
+++ resources/views/backend/product/edit.blade.php
@@ -46,7 +46,9 @@
                                         <select name="cat_id" class="form-control">
                                             <option selected disabled>Select Category</option>
                                             @foreach ($categories as $category)
-                                                <option value="{{$category->id}}">{{$category->name}}</option>
+                                                <option value="{{$category->id}}" @if($category->id == $product->cat_id)
+                                                    selected
+                                                    @endif>{{$category->name}}</option>
                                             @endforeach
                                         </select>
                                     </div>
@@ -55,19 +57,25 @@
                                         <select name="sub_cat_id" class="form-control">
                                             <option selected disabled>Select Category</option>
                                             @foreach ($subCategories as $subcategory)
-                                                <option value="{{$subcategory->id}}">{{$subcategory->name}}</option>
+                                                <option value="{{$subcategory->id}}" @if ($subcategory->id == $product->sub_cat_id)
+                                                    selected
+                                                @endif>{{$subcategory->name}}</option>
                                             @endforeach
                                         </select>
                                     </div>
                                     <div class="form-group" id="color_fields">
                                         <label for="name">Product Color (Optional)</label>
-                                        <input type="text" class="form-control" name="color[]" id="color" placeholder="Enter product color">
+                                        @foreach ($product->color as $singleColor)
+                                            <input type="text" class="form-control" name="color[]" value="{{$singleColor->color_name}}" id="color" placeholder="Enter product color">
+                                        @endforeach
                                     </div>
                                     <button type="button" class="btn btn-primary" id="add_color">Add More</button>
 
                                     <div class="form-group" id="size_fields">
                                         <label for="name">Product Size (Optional)</label>
-                                        <input type="text" class="form-control" name="size[]" id="size" placeholder="Enter product size">
+                                        @foreach ($product->size as $singleSize)
+                                            <input type="text" class="form-control" name="size[]" value="{{$singleSize->size_name}}" id="size" placeholder="Enter product size">
+                                        @endforeach
                                     </div>
                                     <button type="button" class="btn btn-primary" id="add_size">Add More</button>
                                     <div class="form-group">
@@ -99,10 +107,18 @@
                                         <label for="name">Select Product Type*</label>
                                         <select name="product_type" class="form-control">
                                             <option selected disabled>Select Type*</option>
-                                            <option value="hot">Hot Products</option>
-                                            <option value="new">New Arrival</option>
-                                            <option value="regular">Regular Products</option>
-                                            <option value="discount">Discount Products</option>
+                                            <option value="hot" @if ($product->product_type == "hot")
+                                                selected
+                                            @endif>Hot Products</option>
+                                            <option value="new" @if ($product->product_type == "new")
+                                                selected
+                                            @endif>New Arrival</option>
+                                            <option value="regular" @if ($product->product_type == "regular")
+                                                selected
+                                            @endif>Regular Products</option>
+                                            <option value="discount" @if ($product->product_type == "discount")
+                                                selected
+                                            @endif>Discount Products</option>
                                         </select>
                                     </div>
                                     <div class="form-group">
@@ -123,13 +139,17 @@
                                         <label for="exampleInputFile">Gallery Image*</label>
                                         <div class="input-group">
                                             <div class="custom-file">
-                                                <input type="file" class="custom-file-input" name="galleryImage[]" multiple id="galleryImage" accept="image/*" required>
+                                                <input type="file" class="custom-file-input" name="galleryImage[]" multiple id="galleryImage" accept="image/*">
                                                 <label class="custom-file-label" for="image">Choose file</label>
                                             </div>
+                                            
                                             <div class="input-group-append">
                                                 <span class="input-group-text">Upload</span>
                                             </div>
                                         </div>
+                                        @foreach ($product->galleryImage as $image)
+                                            <img src="{{asset('backend/images/gallerImage/'.$image->image)}}" height="100" width="100">
+                                        @endforeach
                                     </div>
                                 </div>
                                 <!-- /.card-body -->
